8a42fe3e574b38bf6bd936277feb001c23688883
Mili.F <milagrosfarfan2000@gmail.com>
2026-01-26 17:51:35 -0300

    Merge branch 'develop' of https://github.com/learning-codes-projects/django-single into develop

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "users",
     "register",
     "login",
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
--- register/views.py
+++ register/views.py
@@ -7,17 +7,19 @@ from django.db import transaction
 
 logger = logging.getLogger(__name__)
 
-class RegisterView(FormView):
 
+class RegisterView(FormView):
     template_name = "register/index.html"
     form_class = UserCreationForm
-    success_url = reverse_lazy("dashboard:index")  # Redirige al formulario de registro después de guardar
+    success_url = reverse_lazy(
+        "dashboard:index"
+    )  # Redirige al formulario de registro después de guardar
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Register"
         return context
-    
+
     def form_valid(self, form):
         try:
             logger.info(f"Formulario válido. Datos: {form.cleaned_data}")
@@ -28,9 +30,7 @@ class RegisterView(FormView):
         except Exception as e:
             logger.error(f"Error al guardar usuario: {str(e)}", exc_info=True)
             raise
-    
+
     def form_invalid(self, form):
         logger.warning(f"Formulario inválido. Errores: {form.errors}")
         return super().form_invalid(form)
-    
-
