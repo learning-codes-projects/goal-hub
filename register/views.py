9e006495d3621273783ad5888c076ba2f04f6a88
Mili.F <milagrosfarfan2000@gmail.com>
2026-01-27 19:05:08 -0300

    chore: auth deleted & register route modified

--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import redirect
 logger = logging.getLogger(__name__)
 
 
-class RegisterView(LoginRequiredMixin, FormView):
+class RegisterView( FormView):
     template_name = "register/index.html"
     form_class = UserCreationForm
     success_url = reverse_lazy(
